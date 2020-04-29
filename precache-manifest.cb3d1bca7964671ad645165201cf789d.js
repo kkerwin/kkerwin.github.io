35124239a4f6f8e41c5998416988f05410c91743
kerwin <kkerwin@yeah.net>
2020-04-29 17:36:23 +0800

    manifest

---
+++ precache-manifest.cb3d1bca7964671ad645165201cf789d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da91a5a785ba1542ca0cbfefcb58455b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf5a208be68cef8607b",
+    "url": "/static/css/main.0e1f6f9e.chunk.css"
+  },
+  {
+    "revision": "ccb11f59657168deaf40",
+    "url": "/static/js/2.723a730c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.723a730c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf5a208be68cef8607b",
+    "url": "/static/js/main.32f384f4.chunk.js"
+  },
+  {
+    "revision": "816c1b2a1a89b4a7dbde",
+    "url": "/static/js/runtime-main.ef53bdac.js"
+  }
+]);
